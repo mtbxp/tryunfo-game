5eb86acf7018b863f2bdee48f204fce283de0250
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-24 17:10:07 -0300

    crianco o checkbox

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Checkbox extends Component {
+  render() {
+    return (
+      <checkbox data-testid="trunfo-input">Super Trunfo</checkbox>
+    );
+  }
+}
