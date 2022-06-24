2cdbbef59ed697d8cced1fe827e601b41111ecb5
leovcampos <leonardoviniciuscampos@gmail.com>
2022-06-24 19:31:06 -0300

    tentando rquisito 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
+import './styles/generic.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+    };
+  }
+
+  onInputChange = (event) => {
+    const { name } = event;
+    const value = event.type === 'checkbox' ? event.checked : event.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
     return (
       <div>
-        <h1>Tryunfo 1</h1>
-        <Form />
-        <Card />
+        <h1>Tryunfo</h1>
+        <Form
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
