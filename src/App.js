f6b3f4e50425d7d0c5e0e34f3fb2508ae58c06e1
Kauan <kauan.s.leite@hotmail.com>
2022-06-24 16:45:00 -0300

    3° Requisito

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
@@ -19,6 +20,17 @@ class App extends React.Component {
           isSaveButtonDisabled={ false }
           onInputChange={ () => {} }
           onSaveButtonClick={ () => {} }
+        />
+
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
         />
       </div>
     );
---
+++ src/components/Card.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2,
+      cardAttr3, cardImage, cardRare, cardTrunfo,
+    } = this.props;
+
+    const superTrunfo = cardTrunfo === true
+      ? <p data-testid="trunfo-card">Super Trunfo</p> : '';
+
+    return (
+      <>
+        <h1>Card</h1>
+
+        <h3 data-testid="name-card">{ cardName }</h3>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <h3 data-testid="attr1-card">{ cardAttr1 }</h3>
+        <h3 data-testid="attr2-card">{ cardAttr2 }</h3>
+        <h3 data-testid="attr3-card">{ cardAttr3 }</h3>
+        <p data-testid="rare-card">{ cardRare }</p>
+        { superTrunfo }
+      </>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
