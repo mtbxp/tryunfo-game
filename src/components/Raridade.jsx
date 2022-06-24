d1fe612be55d785d0a8dcfd1cc48fdb21a2ef997
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-24 16:52:42 -0300

    criando campo para inserir raridade da carta

---
+++ src/components/Raridade.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+const rarityLetter = ['normal', 'raro', 'muito raro'];
+
+export default class Raridade extends Component {
+  render() {
+    const { func, value } = this.props;
+
+    return (
+      <label htmlFor="name">
+        Raridade:
+        <select id="name">
+          <option value="">Selecione</option>
+          {
+            rarityLetter.map((letter, key) => (
+              <option key={ key }>{letter}</option>
+            ))
+          }
+          <input
+            type="text"
+            data-testid="name-input"
+            name="name"
+            id="name"
+            value={ value }
+            onChange={ func }
+            required
+          />
+        </select>
+      </label>
+    );
+  }
+}
+
+Raridade.propTypes = {
+  func: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
