3e8b9a7e1120e956eda8102489faf07b0cfd68f5
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-24 15:37:12 -0300

    criando input nome

---
+++ src/components/Nome.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Nome extends Component {
+  render() {
+    const { func, value } = this.props;
+    return (
+      <label htmlFor="name">
+        Nome:
+        <input
+          type="text"
+          data-testid="name-input"
+          name="nome"
+          id="name"
+          value={ value }
+          onChange={ func }
+          required
+        />
+      </label>
+    );
+  }
+}
+
+Nome.propTypes = {
+  func: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
