5e3cd84d07d36dcfcb7f0bef0a4d175f9862aabe
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-24 18:27:44 -0300

    renomeando e refatorando

---
+++ src/components/Forca.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Forca extends Component {
+  render() {
+    const { cardAttr2, onInputChange } = this.props;
+
+    return (
+      <label htmlFor="attr2-input">
+        Força:
+        <input
+          name="cardAttr2"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
+          id="attr2-input"
+          data-testid="attr2-input"
+          type="number"
+          min="0"
+        />
+      </label>
+    );
+  }
+}
+
+Forca.propTypes = {
+  cardAttr2: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
