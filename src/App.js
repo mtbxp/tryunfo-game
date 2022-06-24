62056653aa2887ff67c3f742d90834deb6ddadc8
Breno Nascimento <satsukebare23@gmail.com>
2022-06-24 17:01:06 -0300

    colocando o defaultProps dentro do state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
-  render() {
-    const defaultProps = {
-      cardName: 'Nome da carta',
-      cardDescription: 'Descrição da carta',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
-      cardImage: 'url-to-image',
-      cardRare: 'raro',
-      cardTrunfo: true,
-      hasTrunfo: false,
-      isSaveButtonDisabled: false,
-      onInputChange: () => { },
-      onSaveButtonClick: () => { },
+  constructor() {
+    super();
+
+    this.state = {
+      defaultProps: {
+        cardName: '',
+        cardDescription: '',
+        cardAttr1: 0,
+        cardAttr2: 0,
+        cardAttr3: 0,
+        cardImage: '',
+        cardRare: 'raro',
+        cardTrunfo: true,
+        hasTrunfo: false,
+        isSaveButtonDisabled: false,
+        onInputChange: () => { },
+        onSaveButtonClick: () => { },
+      },
     };
+  }
+
+  render() {
+    const { defaultProps } = this.state;
     return (
       <div>
         <Form { ...defaultProps } />
+        <Card { ...defaultProps } />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
+import './FormStyles.css';
 
 class Form extends React.Component {
   render() {
