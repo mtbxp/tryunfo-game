dc1f64cb39132bf1f000809522fbcbebec5b5a39
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-06-24 17:22:45 -0300

    Pequenas alterações, importação do arquivo css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
-import './index.css';
+import './App.css';
 
 class App extends React.Component {
   render() {
