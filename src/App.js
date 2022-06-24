531be8eb0967a33a7a0c93e62c943128cf506634
Lucas <lucaschmain@gmail.com>
2022-06-24 17:14:42 -0300

    concluindo requisito 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import { bool } from 'prop-types';
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <Form
-        cardName=''
-        cardDescription=''
-        cardAttr1=''
-        cardAttr2=''
-        cardAttr3=''
-        cardImage=''
-        cardRare=''
-        cardTrunfo={false}
-        hasTrunfo={false}
-        isSaveButtonDisabled={false}
-        onInputChange={() => {}}
-        onSaveButtonClick={() => {}}
+      <>
+        <Form
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ bool }
+          hasTrunfo={ bool }
+          isSaveButtonDisabled={ bool }
+          onInputChange={ () => {} }
+          onSaveButtonClick={ () => {} }
         />
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ bool }
+        />
+      </>
+
     );
   }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -132,4 +132,19 @@ class Form extends React.Component {
   }
 }
 
+// Form.propTypes = {
+//   cardName: PropTypes.string.isRequired,
+//   cardDescription: PropTypes.string.isRequired,
+//   cardAttr1: PropTypes.string.isRequired,
+//   cardAttr2: PropTypes.string.isRequired,
+//   cardAttr3: PropTypes.string.isRequired,
+//   cardImage: PropTypes.string.isRequired,
+//   cardRare: PropTypes.string.isRequired,
+//   cardTrunfo: PropTypes.bool.isRequired,
+//   hasTrunfo: PropTypes.bool.isRequired,
+//   isSaveButtonDisabled: PropTypes.bool.isRequired,
+//   onInputChange: PropTypes.func.isRequired,
+//   onSaveButtonClick: PropTypes.func.isRequired,
+// };
+
 export default Form;
