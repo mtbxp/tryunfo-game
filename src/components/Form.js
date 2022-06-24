50deb8b30abc753951a04b27bd3f4be00b2549ac
andre09999 <andre09999@live.com>
2022-06-24 15:15:58 -0400

    exercicio 1

---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" data-testid="name-input" />
+        <input type="textarea" data-testid="description-input" />
+        <input type="number" data-testid="attr1-input" />
+        <input type="number" data-testid="attr2-input" />
+        <input type="text" data-testid="image-input" />
+        <select type="select" data-testid="rare-input">
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
+        </select>
+        <input type="checkbox" data-testid="trunfo-input" />
+        <input type="button" data-testid="save-button" />
+      </form>
+    );
+  }
+}
+
+export default Form;
