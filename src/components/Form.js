03b193af8401900f9d72cdebd44415169b74b197
JorgeKz <jorgekzbra@gmail.com>
2022-06-24 17:03:19 -0300

    Implementando requisito 02

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,71 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
+
     return (
       <>
         <input
           type="text"
           name="cardName"
           data-testid="name-input"
+          value={ cardName }
+          onChange={ onInputChange }
         />
         <textarea
           name="cardDescription"
           data-testid="description-input"
+          value={ cardDescription }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           name="cardAtribute1"
           data-testid="attr1-input"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           name="cardAtribute2"
           data-testid="attr2-input"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
           name="cardAtribute3"
           data-testid="attr3-input"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
         />
         <input
           type="text"
           name="cardUrl"
           data-testid="image-input"
+          value={ cardImage }
+          onChange={ onInputChange }
         />
         <select
           name="cardRarity"
           data-testid="rare-input"
+          value={ cardRare }
+          onChange={ onInputChange }
         >
           <option value="normal">Normal</option>
           <option value="raro">Raro</option>
@@ -45,10 +75,14 @@ class Form extends Component {
           type="checkbox"
           name="cardTrunfo"
           data-testid="trunfo-input"
+          checked={ cardTrunfo }
+          onChange={ onInputChange }
         />
         <button
           type="submit"
           data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
         >
           Salvar
         </button>
@@ -56,5 +90,20 @@ class Form extends Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.bool.isRequired,
+  onSaveButtonClick: PropTypes.bool.isRequired,
+};
 
 export default Form;
