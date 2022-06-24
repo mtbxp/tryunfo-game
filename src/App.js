5db17cdb9f087a4f7cef45e420a1b24f441be175
Danilo Leão <olin_ad@hotmail.com>
2022-06-24 19:17:00 -0300

    push mini bomba. Me afobei testando os requisitos e n dei push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
-// import Card from './components/Card';
+import Card from './components/Card';
 
 class App extends React.Component {
   constructor() {
@@ -17,22 +17,91 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
-      // cardsArray: [],
+      cardsArray: [],
       // onInputChange: this.onInputChange,
       // onSaveButtonClick: this.onSaveButtonClick,
     };
-  }
 
-  onInputChange() {
-    return (
-      0
-    );
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.verifySaveButton = this.verifySaveButton.bind(this);
+    this.verifyHasTrunfo = this.verifyHasTrunfo.bind(this);
   }
 
   onSaveButtonClick() {
+    const { cardAttr1, cardAttr2, cardAttr3, cardDescription, cardImage, cardRare,
+      cardName, cardTrunfo } = this.state;
     return (
-      1
+      this.setState((prevState) => ({
+        cardsArray: [
+          ...prevState.cardsArray,
+          {
+            cardName,
+            cardDescription,
+            cardAttr1,
+            cardAttr2,
+            cardAttr3,
+            cardImage,
+            cardRare,
+            cardTrunfo,
+          },
+        ],
+        cardName: '',
+        cardDescription: '',
+        cardAttr1: 0,
+        cardAttr2: 0,
+        cardAttr3: 0,
+        cardImage: '',
+        cardRare: 'normal',
+        cardTrunfo: false,
+        isSaveButtonDisabled: true,
+        // hasTrunfo: this.verifyHasTrunfo()
+      }), () => this.verifyHasTrunfo())
     );
+  }
+
+  onInputChange(event) {
+    console.log('Inputchange')
+    const { target } = event;
+    if (target.name !== 'cardTrunfo') this.verifySaveButton();
+
+    return (
+      this.setState({
+        [target.name]: target.value,
+      })
+    );
+  }
+
+  verifySaveButton() {
+    const { cardName, cardDescription, cardImage, cardRare, cardAttr1, cardAttr2,
+      cardAttr3 } = this.state;
+    const isEmpty = [cardName, cardDescription, cardImage, cardRare]
+      .map((item) => item.length === 0);
+    const isOver210 = [cardAttr1, cardAttr2, cardAttr3]
+      .reduce((acc, current) => parseInt(acc, 10) + parseInt(current, 10));
+    const maxValue90 = 90;
+    const maxValue210 = 210;
+    const isValid = [cardAttr1, cardAttr2, cardAttr3]
+      .map((item) => item > maxValue90 || item < 0);
+
+    // console.log('attr1', cardAttr1)
+    // console.log('isEmpty: ', isEmpty, 'isOver210', isOver210, 'isOver90', isValid)
+
+    if (!isEmpty.includes(true) && !isValid.includes(true) && isOver210 < maxValue210) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
+  }
+
+  verifyHasTrunfo() {
+    const { cardsArray } = this.state;
+    console.log(cardsArray.some((card) => card.cardTrunfo));
+    if (cardsArray.some((card) => card.cardTrunfo)) {
+      this.setState({
+        hasTrunfo: true,
+      });
+    }
   }
 
   render() {
@@ -43,9 +112,9 @@ class App extends React.Component {
         <Form
           cardName={ cardName }
           cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
+          cardAttr1={ (cardAttr1) }
+          cardAttr2={ (cardAttr2) }
+          cardAttr3={ (cardAttr3) }
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
@@ -53,6 +122,16 @@ class App extends React.Component {
           isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ (cardAttr1) }
+          cardAttr2={ (cardAttr2) }
+          cardAttr3={ (cardAttr3) }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </div>
     );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,100 @@
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Card extends React.Component {
+class Card extends React.Component {
+  render() {
+    const { cardAttr1, cardAttr2, cardAttr3, cardDescription, cardImage, cardRare,
+      cardName, cardTrunfo } = this.props;
 
-// }
+    const HTMLElements = [
+      {
+        Tag: 'h2',
+        name: 'cardName',
+        dataTestid: 'name-card',
+        inner: cardName,
+      },
+      {
+        Tag: 'p',
+        name: 'cardDescription',
+        dataTestid: 'description-card',
+        inner: cardDescription,
+      },
+      {
+        Tag: 'p',
+        name: 'cardAttr1',
+        dataTestid: 'attr1-card',
+        inner: cardAttr1,
+      },
+      {
+        Tag: 'p',
+        name: 'cardAttr2',
+        dataTestid: 'attr2-card',
+        inner: cardAttr2,
+      },
+      {
+        Tag: 'p',
+        name: 'cardAttr3',
+        dataTestid: 'attr3-card',
+        inner: cardAttr3,
+      },
+      {
+        Tag: 'img',
+        name: 'cardImage',
+        dataTestid: 'image-card',
+        src: cardImage,
+        alt: cardName,
+      },
+      {
+        Tag: 'h2',
+        name: 'cardRare',
+        dataTestid: 'rare-card',
+        inner: cardRare,
+      },
+      {
+        Tag: 'h3',
+        name: 'cardTrunfo',
+        dataTestid: 'trunfo-card',
+        trunfo: cardTrunfo,
+        inner: 'Super Trunfo',
+      },
+    ];
+    // console.log(this.props.cardTrunfo)
+    return (
+      <div>
+        { HTMLElements.map(({ Tag, name, dataTestid, trunfo, inner }, index) => (
+          <Tag
+            key={ index }
+            data-testid={ dataTestid }
+            name={ name }
+          >
+            {Tag !== 'h3' || trunfo ? inner : null}
+          </Tag>
+        )) }
+      </div>
+    );
+  }
+}
 
-// export default Card;
+Card.defaultProps = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  cardRare: PropTypes.string,
+};
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  cardRare: PropTypes.string,
+};
+
+export default Card;
