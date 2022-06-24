03a8a586a587475b4de5b6ae40b11654615f1ff5
Guilherme Crupi <guilhermecrupi@hotmail.com>
2022-06-24 17:20:15 -0300

    Requisito 1

---
+++ src/components/Form.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="name">
+          Nome
+          <input type="text" name="name" id="name" data-testid="name-input" />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <input
+            type="textarea"
+            name="description"
+            id="description"
+            data-testid="description-input"
+          />
+        </label>
+        <label htmlFor="attribute1">
+          Atributo 1
+          <input
+            type="number"
+            name="attribute1"
+            id="attribute1"
+            data-testid="attr1-input"
+          />
+        </label>
+        <label htmlFor="attribute2">
+          Atributo 2
+          <input
+            type="number"
+            name="attribute2"
+            id="attribute2"
+            data-testid="attr2-input"
+          />
+        </label>
+        <label htmlFor="attribute3">
+          Atributo 3
+          <input
+            type="number"
+            name="attribute3"
+            id="attribute3"
+            data-testid="attr3-input"
+          />
+        </label>
+        <label htmlFor="image">
+          Imagem
+          <input
+            type="text"
+            name="image"
+            id="image"
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="rare">
+          Raridade
+          <input type="select" name="rare" id="rare" data-testid="rare-input" />
+        </label>
+        <label htmlFor="check-trunfo">
+          Trunfo
+          <input
+            type="checkbox"
+            name="check-trunfo"
+            id="check-trunfo"
+            data-testid="trunfo-input"
+          />
+        </label>
+        <button
+          type="submit"
+          name="button"
+          id="button"
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
