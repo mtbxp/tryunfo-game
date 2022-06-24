cfd295644a458988db0e85339e27571d3759a693
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-06-24 18:35:01 -0300

    Retirando o erro de lint com o hastrunfo que ainda n foi usado e inciando o 3 reqisito.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
@@ -7,6 +8,33 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
+        <div>
+          <p data-testid="name-card">{ cardName }</p>
+          <img
+            data-testid="image-card"
+            src={ cardImage }
+            alt={ cardName }
+          />
+          <p data-testid="description-card">
+            { cardDescription }
+          </p>
+          <p data-testid="attr1-card">
+            { cardAttr1 }
+          </p>
+          <p data-testid="attr2-card">
+            { cardAttr2 }
+          </p>
+          <p data-testid="attr3-card">
+            { cardAttr3 }
+          </p>
+          <p data-testid="rare-card">
+            { cardRare }
+          </p>
+          <p data-testid="trunfo-card">
+            { cardTrunfo }
+          </p>
+        </div>
       </div>
     );
   }
