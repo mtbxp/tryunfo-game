fc71c2e7220f1c34409998bfbc313f3aef537dfd
Luan Victor <luanvictor2108@gmail.com>
2022-06-24 17:33:35 -0300

    Resolvendo requesito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'; // exodia obliterar
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -7,6 +8,16 @@ class App extends React.Component {
       <div>
         <h1 id="title">Tryunfo</h1>
         <Form />
+        <Card
+          cardName="Omori"
+          cardImage="https://preview.redd.it/zs47l46zh1j61.gif?format=png8&s=3b7763fda35062c74f47324e8a3f15ab2a31b927"
+          cardDescription="Alterego de Sunny"
+          cardAttr1={ 70 }
+          cardAttr2={ 90 }
+          cardAttr3={ 60 }
+          cardRare="raro"
+          cardTrunfo={ true }
+        />
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import '../Css/Card.css'
 
 class Card extends React.Component {
-  render() {
-    return (
+  constructor(){
+      super();
 
+      this.isSuperTrunfo = this.isSuperTrunfo.bind(this)
+  }
+
+  isSuperTrunfo() {
+    if(this.props.cardTrunfo === true) {
+        return (
+            <p id="super-trunfo" data-testid="trunfo-card">Super Trunfo</p>
+        );
+    }
+  }
+
+  render() {
+      const {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+      } = this.props
+    return (
+      <section id="section-card">
+          <h3 id='title' data-testid="name-card">{ cardName }</h3>
+          <img id="card-image" data-testid="image-card" src={ cardImage } alt={ cardName } />
+          <p data-testid="description-card">{ cardDescription }</p>
+          <div id="card-infos">
+          <p data-testid="attr1-card">{ cardAttr1 }</p>
+          <p data-testid="attr2-card">{ cardAttr2 }</p>
+          <p data-testid="attr3-card">{ cardAttr3 }</p>
+          <p data-testid="rare-card">{ cardRare }</p>
+          </div>
+          <div>{ this.isSuperTrunfo() }</div>
+      </section>
     );
   }
 }
+
+export default Card;
