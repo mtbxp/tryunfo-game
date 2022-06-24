f4ab533219738f29ddfbc793476c46f854ac8e02
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-24 18:30:43 -0300

    renomeando e refatorando

--- src/components/Vida.jsx
+++ src/components/Vida.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-export default class Atributo3 extends Component {
+export default class Vida extends Component {
   render() {
-    const { func, value } = this.props;
+    const { cardAttr3, onInputChange } = this.props;
 
     return (
-      <label htmlFor="number">
-        Atributo 3:
+      <label htmlFor="attr3-input">
+        Vida:
         <input
-          type="number"
+          name="cardAttr3"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
+          id="attr3-input"
           data-testid="attr3-input"
-          name="number"
-          id="number"
-          value={ value }
-          onChange={ func }
-          required
+          type="number"
+          min="0"
         />
       </label>
     );
   }
 }
 
-Atributo3.propTypes = {
-  func: PropTypes.func.isRequired,
-  value: PropTypes.number.isRequired,
+Vida.propTypes = {
+  cardAttr3: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
 };
