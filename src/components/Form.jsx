742758dcf59053f14b0c4d320ff91824e3e83c56
Matheus Liberato <mtvargas42@gmail.com>
2022-06-25 00:57:02 -0300

    requisito 2 (hasTrunfo comentado)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,7 @@ class Form extends React.Component {
     const { cardImage } = this.props;
     const { cardRare } = this.props;
     const { cardTrunfo } = this.props;
-    const { hasTrunfo } = this.props;
+    // const { hasTrunfo } = this.props;
     const { isSaveButtonDisabled } = this.props;
     const { onInputChange } = this.props;
     const { onSaveButtonClick } = this.props;
@@ -124,7 +124,7 @@ Form.propTypes = {
   cardImage: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
