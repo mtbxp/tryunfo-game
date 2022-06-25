1f8063ca1a1295a80c19696bd0a1d14c875af51c
JorgeKz <jorgekzbra@gmail.com>
2022-06-25 19:33:56 -0300

    Implementando requisito 06

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
+      cards: [],
     };
   }
 
@@ -29,20 +30,55 @@ class App extends React.Component {
     } = this.state;
     const maximumScore = 210;
     const maxSkillScore = 90;
+
     const totalScoreOfSkills = ((Number(cardAttr1) <= maxSkillScore)
       && (Number(cardAttr1) >= 0))
       && ((Number(cardAttr2) <= maxSkillScore)
       && (Number(cardAttr2) >= 0))
       && ((Number(cardAttr3) <= maxSkillScore)
       && (Number(cardAttr3) >= 0));
+
     const allScore = (Number(cardAttr1)
       + Number(cardAttr2)
       + Number(cardAttr3)) <= maximumScore;
+
     return !(!!cardName
       && cardDescription && cardImage && cardRare && allScore && totalScoreOfSkills);
   }
 
-  onSaveButtonClick = () => false
+  onSaveButtonClick = (event) => {
+    event.preventDefault();
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
+    this.setState(({ cards: prevList }) => ({
+      cards: [...prevList, {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+        cardTrunfo,
+      }],
+    }), () => this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: '',
+    }));
+  }
 
   onInputChange = (event) => {
     const { target } = event;
