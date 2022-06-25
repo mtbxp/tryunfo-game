b5a421782cb8c058530cdfeabc6055cce4a49908
lucaseduardolema <lucaseduardolema@gmail.com>
2022-06-25 14:48:06 -0300

    add req 5

--- src/App.js
+++ src/App.js
@@ -13,11 +13,37 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: true,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
+  }
+
+  validaCampos = (value1, value2, value3) => {
+    const valuesOfState = Object.values(this.state);
+    value1 = parseInt(value1, 10);
+    value2 = parseInt(value2, 10);
+    value3 = parseInt(value3, 10);
+    const maxPoints = 90;
+    const totalPoints = 210;
+    const somaValues = value1 + value2 + value3;
+    if (valuesOfState.includes('')
+      || value1 < 0
+      || value1 > maxPoints
+      || value2 < 0
+      || value2 > maxPoints
+      || value3 < 0
+      || value3 > maxPoints
+      || somaValues > totalPoints) {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
   }
 
   onInputChange = ({ target }) => {
@@ -27,6 +53,15 @@ class App extends React.Component {
     if (type === 'checkbox') value = hasTrunfo;
     this.setState({
       [name]: value,
+    }, () => {
+      const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+      let valueAtt1 = cardAttr1;
+      let valueAtt2 = cardAttr2;
+      let valueAtt3 = cardAttr3;
+      if (name === 'cardAttr1') valueAtt1 = value;
+      if (name === 'cardAttr2') valueAtt2 = value;
+      if (name === 'cardAttr3') valueAtt3 = value;
+      this.validaCampos(valueAtt1, valueAtt2, valueAtt3);
     });
   }
 
