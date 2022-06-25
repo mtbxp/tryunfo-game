5f820679ed3796c4277ecb96816dae1eb67a7cd5
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-25 09:21:37 -0300

    excluindo hasTrunfo por enquanto

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ import '../styles/form.css';
 export default class Form extends Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled,
       onInputChange, onSaveButtonClick } = this.props;
 
     return (
@@ -158,7 +158,6 @@ Form.propTypes = {
   cardName: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
-  hasTrunfo: PropTypes.bool.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
