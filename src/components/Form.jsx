9606b2969a2a775ea150ddc60c101e7ae35c7fb4
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-25 09:06:34 -0300

    inserindo a props hasTrunfo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,8 +6,8 @@ import '../styles/form.css';
 export default class Form extends Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled, onInputChange,
-      onSaveButtonClick } = this.props;
+      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick } = this.props;
 
     return (
       <form className="form">
@@ -158,6 +158,7 @@ Form.propTypes = {
   cardName: PropTypes.string.isRequired,
   cardRare: PropTypes.string.isRequired,
   cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
