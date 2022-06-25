bc53bf2e8cadca97e3a4bc96f6faa47326c77408
danielrubens <danielrubensdaniel@gmail.com>
2022-06-25 18:39:31 -0300

    05

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.onInputChange = this.onInputChange.bind(this);
+    this.infoFormCorrect = this.infoFormCorrect.bind(this);
+    this.sumAttributes = this.sumAttributes.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
     this.state = {
       cardName: '',
       cardDescription: '',
@@ -14,14 +17,49 @@ class App extends React.Component {
       cardAttr3: '',
       cardImage: '',
       cardRare: '',
-      cardTrunfo: '' };
+      cardTrunfo: '',
+      isSaveButtonDisabled: true };
+  }
+
+
+  infoFormCorrect(info) {
+    return info.every((i) => i.length > 0);
+  }
+
+  sumAttributes(attrs) {
+    const maxAttribute = 210;
+    const maxValue = 90;
+    const value = attrs.reduce((acc, cur) => Number(acc) + Number(cur), 0);
+    if (value > maxAttribute) {
+      return false;
+    }
+    if (attrs.some((i) => Number(i) > maxValue || Number(i) < 0)) {
+      return false;
+    }
+    return true;
   }
 
   onInputChange({ target }) {
     const { name } = target;
     const { value } = target;
-    this.setState(({ [name]: value }));
+
+    this.setState(({ [name]: value }), () => {
+      this.returnIsSaveButton();
+    });
   }
+
+  returnIsSaveButton = () => {
+    const { cardName, cardDescription, cardAttr1, cardAttr2,
+      cardAttr3, cardImage, cardRare } = this.state;
+    const infoFormValidate = this
+      .infoFormCorrect([cardName, cardDescription, cardImage, cardRare]);
+    const sumAttributesValidate = this.sumAttributes([cardAttr1, cardAttr2, cardAttr3]);
+    const validate = infoFormValidate && sumAttributesValidate;
+    if (validate) {
+      this.setState({ isSaveButtonDisabled: false });
+    }
+  }
+
 
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
