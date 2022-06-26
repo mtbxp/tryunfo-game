4ac8b8d52927239e6315e7611b06ce467c924876
Ricardo <ricardo_md_25@hotmail.com>
2022-06-26 10:51:45 -0300

    exercise 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
 
-
 class App extends React.Component {
   render() {
     return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,71 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <form>
-        <label htmlFor="name">
-          <input
-            type="text"
-            data-testid="name-input"
-          />
-        </label>
-        <label htmlFor="description">
-          <input
-            type="textarea"
-            data-testid="description-input"
-          />
-        </label>
-      </form>
+      <div className="form">
+        <form>
+          <label htmlFor="name">
+            name
+            <input
+              type="text"
+              data-testid="name-input"
+            />
+          </label>
+          <label htmlFor="description">
+            description
+            <input
+              type="textarea"
+              data-testid="description-input"
+            />
+          </label>
+          <label htmlFor="attr1">
+            attr1
+            <input
+              type="number"
+              data-testid="attr1-input"
+            />
+          </label>
+          <label htmlFor="attr2">
+            attr2
+            <input
+              type="number"
+              data-testid="attr2-input"
+            />
+          </label>
+          <label htmlFor="attr3">
+            attr3
+            <input
+              type="number"
+              data-testid="attr3-input"
+            />
+          </label>
+          <label htmlFor="image">
+            image
+            <input
+              type="text"
+              data-testid="image-input"
+            />
+          </label>
+          <label htmlFor="rare">
+            rare
+            <select
+              type="select"
+              data-testid="rare-input"
+            >
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+          <label htmlFor="trunfo">
+            trunfo
+            <input
+              type="checkbox"
+              data-testid="trunfo-input"
+            />
+          </label>
+          <button type="button" data-testid="save-button">Salvar</button>
+        </form>
+      </div>
     );
   }
 }
