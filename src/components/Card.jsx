1b5f49342c4fd6526ed51b7f54bbe15f6dd1a363
leandro-miranda <leandromiranda2010@gmail.com>
2022-06-26 11:22:17 -0300

    arrumando o cardTrunfo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,9 +12,8 @@ export default class Card extends Component {
         <p data-testid="attr1-card">{cardAttr1}</p>
         <p data-testid="attr2-card">{cardAttr2}</p>
         <p data-testid="attr3-card">{cardAttr3}</p>
-        <p data-testid="rare-card">{cardRare}</p>
         <img data-testid="image-card" src={ cardImage } alt={ cardName } />
-        <p data-testid="trunfo-card">{cardTrunfo}</p>
+        <p data-testid="rare-card">{cardRare}</p>
 
         {cardTrunfo && <p data-testid="trunfo-card">Super Trunfo!</p> }
       </div>
