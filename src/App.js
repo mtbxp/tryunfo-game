10a50d109cfbb919b3fe22e4adaeb16f20f73884
Manuela França <manuelaalves26@gmail.com>
2022-06-26 15:49:40 -0300

    Requisito 6.

--- src/App.js
+++ src/App.js
@@ -16,10 +16,12 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       enableButton: false,
+      listOfCards: [],
       isSaveButtonDisabled: true,
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
     this.validationButton = this.validationButton.bind(this);
   }
 
@@ -27,6 +29,25 @@ class App extends React.Component {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({ [name]: value }, this.validationButton);
+  }
+
+  onSaveButtonClick(event) {
+    event.preventDefault();
+
+    const newCard = { ...this.state };
+
+    this.setState((newState) => ({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      listOfCards: [...newState.listOfCards, newCard],
+    }));
   }
 
   validationButton() {
@@ -97,6 +118,7 @@ class App extends React.Component {
           cardTrunfo={ cardTrunfo }
           hasTrunfo={ hasTrunfo }
           isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.onSaveButtonClick }
           onInputChange={ this.onInputChange }
         />
         <Card
