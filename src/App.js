f661926b6b9b4d2dfc7b3f35e07e6725767ac954
JoaoDobrowolski <joaodobrowolski@outlook.com>
2022-06-27 18:45:17 -0300

    1 a 5 ok

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ const INITIAL_STATE = {
   cardAttr2: 0,
   cardAttr3: 0,
   cardImage: '',
-  cardRare: '',
+  cardRare: 'normal',
   cardTrunfo: false,
   hasTrunfo: false,
-  isSaveButtonDisabled: true,
+  isSaveButtonDisabled: false,
 };
 
 class App extends React.Component {
@@ -30,10 +30,36 @@ class App extends React.Component {
     });
   }
 
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  // }
+
+  validateSaveButton = () => {
+    const { cardName, cardImage, cardDescription, cardRare, cardAttr1,
+      cardAttr2, cardAttr3 } = this.state;
+    const attr1ToNumber = parseInt(cardAttr1, 10);
+    const attr2ToNumber = parseInt(cardAttr2, 10);
+    const attr3ToNumber = parseInt(cardAttr3, 10);
+    const maxValue = 210;
+    const maxIndividualValue = 90;
+    if (!cardName.length || !cardImage.length
+      || !cardDescription.length || !cardRare.length) return true;
+    if (attr1ToNumber + attr2ToNumber + attr3ToNumber > maxValue) return true;
+    if (attr1ToNumber < 0 || attr2ToNumber < 0 || attr3ToNumber < 0) return true;
+    if (attr1ToNumber > maxIndividualValue
+      || attr2ToNumber > maxIndividualValue
+      || attr3ToNumber > maxIndividualValue) return true;
+  }
+
   render() {
     return (
       <div className="main-content">
-        <Form { ...this.state } onInputChange={ this.handleChange } />
+        <Form
+          { ...this.state }
+          onInputChange={ this.handleChange }
+          isSaveButtonDisabled={ this.validateSaveButton() }
+        // onSaveButtonClick={ this.handleSubmit }
+        />
         <Card { ... this.state } />
       </div>
     );
