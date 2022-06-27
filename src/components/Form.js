8c716adfec75f987f32aa7b58bdf3c53886e3e33
Matheus <Maths.Aki@gmail.com>
2022-06-27 15:03:55 -0300

    Adicionando as Props ao forms

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,32 +12,67 @@ class Form extends Component {
         <h1><b>Adicionar nova Carta</b></h1>
         <br />
         <br />
-        <b>Nome:</b>
-        <input type="text" data-testid="name-input" />
+        <b className="Nome">Nome:</b>
+        <input
+          type="text"
+          className="name-input"
+          value="cardname"
+          onChance={ onInputChange }
+          data-testid="name-input"
+        />
         <br />
-        <b>Descrição:</b>
-        <input type="textarea" data-testid="description-input" />
+        <b className="Descricao">Descrição:</b>
+        <input
+          type="textarea"
+          className="description-input"
+          value="cardDescription"
+          onChange={ onInputChange }
+          data-testid="description-input"
+        />
         <br />
         <br />
 
-        <b>Primeiro Atributo:</b>
-        <input type="number" data-testid="attr1-input" />
+        <b className="1Atb">Primeiro Atributo:</b>
+        <input
+          type="number"
+          className="attr1-input"
+          value="cardAttr1"
+          onChange={ onInputChange }
+          data-testid="attr1-input"
+        />
         <br />
-        <b>Segundo Atributo:</b>
-        <input type="number" data-testid="attr2-input" />
+        <b className="2Atb">Segundo Atributo:</b>
+        <input
+          type="number"
+          className="attr2-input"
+          value="cardAttr2"
+          onChange={ onInputChange }
+          data-testid="attr2-input"
+        />
         <br />
-        <b>Terceiro Atributo:</b>
-        <input type="number" data-testid="attr3-input" />
+        <b className="3Atb">Terceiro Atributo:</b>
+        <input
+          type="number"
+          className="attr3-input"
+          value="cardAttr3"
+          onChange={ onInputChange }
+          data-testid="attr3-input"
+        />
         <br />
         <br />
 
-        <b>Imagem:</b>
-        <input type="text" data-testid="image-input" />
+        <b className="Imagem">Imagem:</b>
+        <input
+          type="text"
+          className="cardImage"
+          onChange={ onInputChange }
+          data-testid="image-input"
+        />
         <br />
         <br />
 
         <b>Raridade:</b>
-        <select>
+        <select className="rare-input" value="cardRare" onChange={ onInputChange }>
           <option value="normal">Normal</option>
           <option value="raro">Raro</option>
           <option value="muito raro">Muito Raro</option>
@@ -45,12 +80,26 @@ class Form extends Component {
         <br />
         <br />
 
-        <input type="checkbox" data-testid="trunfo-input" />
+        <input
+          type="checkbox"
+          className="trunfo-input"
+          checked="cardTrunfo"
+          onChange={ onInputChange }
+          data-testid="trunfo-input"
+        />
         <b>Super Trunfo!</b>
         <br />
         <br />
 
-        <button type="button" data-testid="save-button">Salvar</button>
+        <button
+          type="button"
+          className="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
       </div>
     );
   }
