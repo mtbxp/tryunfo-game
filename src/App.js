34318ccf10df94225ab023eb7b989312c4ac6318
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-06-27 19:39:03 -0300

    Adicionando o requisito 4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,72 @@ import Form from './components/Form';
 import Card from './components/Card';
 import './App.css';
 
+const initialState = {
+  cardName: '',
+  cardDescription: '',
+  cardAttr1: '',
+  cardAttr2: '',
+  cardAttr3: '',
+  cardImage: '',
+  cardRare: '',
+  cardTrunfo: false,
+  hasTrunfo: false,
+  isSaveButtonDisabled: false,
+};
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  onSaveButtonClick = () => {};
+
+  onInputChange = (event) => {
+    this.setState(
+      {
+        cardName: event.target.value,
+        cardDescription: event.target.value,
+        cardAttr1: event.target.value,
+        cardAttr2: event.target.value,
+        cardAttr3: event.target.value,
+        cardImage: event.target.value,
+        cardRare: event.target.value,
+        cardTrunfo: event.target.value,
+      },
+      () => console.log(event),
+    );
+  };
+
   render() {
+    const value = this.state;
     return (
       <div className="father">
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form
+          cardName={ value.cardName }
+          cardDescription={ value.cardDescription }
+          cardAttr1={ value.cardAttr1 }
+          cardAttr2={ value.cardAttr2 }
+          cardAttr3={ value.cardAttr3 }
+          cardImage={ value.cardImage }
+          cardRare={ value.cardRare }
+          cardTrunfo={ value.cardTrunfo }
+          hasTrunfo={ value.hasTrunfo }
+          isSaveButtonDisabled={ value.isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card
+          cardName={ value.cardName }
+          cardDescription={ value.cardDescription }
+          cardAttr1={ value.cardAttr1 }
+          cardAttr2={ value.cardAttr2 }
+          cardAttr3={ value.cardAttr3 }
+          cardImage={ value.cardImage }
+          cardRare={ value.cardRare }
+          cardTrunfo={ value.cardTrunfo }
+        />
       </div>
     );
   }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,23 +3,26 @@ import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
-    const { cardName } = this.props;
-    const { cardDescription } = this.props;
-    const { cardAttr1 } = this.props;
-    const { cardAttr2 } = this.props;
-    const { cardAttr3 } = this.props;
-    const { cardImage } = this.props;
-    const { cardRare } = this.props;
-    const { cardTrunfo } = this.props;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
     return (
       <div className="card">
-        <h3 data-testid="name-card">{cardName}</h3>
-        <p data-testid="description-card">{cardDescription}</p>
-        <span data-testid="attr1-card">{cardAttr1}</span>
-        <span data-testid="attr2-card">{cardAttr2}</span>
-        <span data-testid="attr3-card">{cardAttr3}</span>
+        <h3 data-testid="name-card">{`Nome: ${cardName}`}</h3>
+        <p data-testid="description-card">{`Descrição: ${cardDescription}`}</p>
+        <span data-testid="attr1-card">{`Atributo 1: ${cardAttr1}`}</span>
+        <span data-testid="attr2-card">{`Atributo 2: ${cardAttr2}`}</span>
+        <span data-testid="attr3-card">{`Atributo 3: ${cardAttr3}`}</span>
         <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <span data-testid="rare-card">{cardRare}</span>
+        <span data-testid="rare-card">{`Raridade: ${cardRare}`}</span>
         <p data-testid="trunfo-card">{cardTrunfo}</p>
       </div>
     );
