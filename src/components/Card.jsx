26552f2bea1e2bb764b19553a55ea577a1f8cdc7
Ana Luiza <analuizasoareis@gmail.com>
2022-06-26 21:37:40 -0300

    requisito 2

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import Form from './Form';
+
+// class Card extends React.Component {
+//   render() {
+//     return(
+//       <Form data-testid="name-card">{ cardName } />
+//     );
+//   }
+// }
+
+// export default Card;
