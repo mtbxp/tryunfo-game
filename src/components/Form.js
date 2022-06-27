4fb845c1f8c0608812d90597b75f528f98ac2bc8
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-06-27 18:39:29 -0300

    arrumei os pedidos do lint

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
@@ -109,9 +110,8 @@ class Form extends React.Component {
           type="submit"
           data-testid="save-button"
           name="isSaveButtonDisabled"
-          value={ isSaveButtonDisabled }
+          disabled={ isSaveButtonDisabled }
           onChange={ onSaveButtonClick }
-          disabled={ false }
         >
           Salvar
         </button>
@@ -119,5 +119,19 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
