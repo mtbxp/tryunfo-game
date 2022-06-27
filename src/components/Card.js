17c8c53bc32bc5d173f625437740f751cb394d45
rubimeryka <erykarubim13@gmail.com>
2022-06-27 17:04:13 -0300

    requisito 3 corrigido

--- src/components/Card.js
+++ src/components/Card.js
@@ -22,11 +22,8 @@ class Card extends React.Component {
         <div data-testid="attr2-card">{ cardAttr2 }</div>
         <div data-testid="attr3-card">{ cardAttr3 }</div>
         <div data-testid="rare-card">{ cardRare }</div>
-        <div data-testid="trunfo-card">
-          { () => { if (cardTrunfo) return cardTrunfo; } }
-        </div>
+        {cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
       </div>
-
     );
   }
 }
