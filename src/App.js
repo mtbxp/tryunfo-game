6e13a032ee8cfc781b1c91711616c762e422492f
Lucas <lucaschmain@gmail.com>
2022-06-27 20:55:41 -0300

    concluindo requisito 5

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
     };
   }
 
@@ -23,13 +22,30 @@ class App extends React.Component {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
 
-    this.setState({
+    this.setState(() => ({
       [name]: value,
-    });
+    }));
   };
+
+  buttonDisabled = () => {
+    const { state } = this;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const objectArr1 = Object.values(state);
+    const objectArr2 = [cardAttr1, cardAttr2, cardAttr3];
+    let bool3 = false;
+    const maxValue = 90;
+    const maxSumValue = 210;
+    const sumValues = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const bool1 = objectArr1.some((el1) => el1 === '');
+    const bool2 = objectArr2.some((el2) => Number(el2) > maxValue || Number(el2) < 0);
+    if (sumValues > maxSumValue) bool3 = true;
+    console.log(bool1, bool2, bool3);
+    return bool1 || bool2 || bool3;
+  }
 
   render() {
     const {
+
       cardName,
       cardDescription,
       cardAttr1,
@@ -39,9 +55,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
-      isSaveButtonDisabled,
     } = this.state;
-
     const { saveStateInput } = this;
     return (
       <>
@@ -55,7 +69,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           hasTrunfo={ hasTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
+          isSaveButtonDisabled={ this.buttonDisabled() }
           onInputChange={ saveStateInput }
           onSaveButtonClick={ () => {} }
         />
