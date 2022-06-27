510d6d16723867529a746a55d0d94cb454393eac
Guilherme Campos <guilhermeregular@gmail.com>
2022-06-27 00:17:25 -0300

    req 10

--- src/App.js
+++ src/App.js
@@ -18,80 +18,83 @@ class App extends React.Component {
       hasTrunfo: false,
       isSaveButtonDisabled: true,
       cards: [],
+      filterInput: '',
     };
   }
 
   onInputChange = (e) => {
     const { name } = e.target;
     const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-    this.setState({
-      [name]: value,
-    }, () => {
-      if (!this.isInputFilled() && !this.sumOfAttr()
-      && !this.verifyAttr() && !this.verifyAttr2()) {
-        this.setState({
-          isSaveButtonDisabled: false,
-        });
-      } else {
-        this.setState({
-          isSaveButtonDisabled: true,
-        });
-      }
-    });
-  }
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        if (
+          !this.isInputFilled()
+          && !this.sumOfAttr()
+          && !this.verifyAttr()
+          && !this.verifyAttr2()
+        ) {
+          this.setState({
+            isSaveButtonDisabled: false,
+          });
+        } else {
+          this.setState({
+            isSaveButtonDisabled: true,
+          });
+        }
+      },
+    );
+  };
 
   isInputFilled = () => {
     const { cardName, cardDescription, cardImage, cardRare } = this.state;
-    if (cardName.length !== 0 && cardDescription.length !== 0
-    && cardImage.length !== 0 && cardRare.length !== 0) {
+    if (
+      cardName.length !== 0
+      && cardDescription.length !== 0
+      && cardImage.length !== 0
+      && cardRare.length !== 0
+    ) {
       return false;
     }
     return true;
-  }
+  };
 
   sumOfAttr = () => {
     const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
     const magic = 210;
-    if ((Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3)) > magic) {
+    if (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) > magic) {
       return true;
     }
     return false;
-  }
+  };
 
   verifyAttr = () => {
     const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
     const magic = 90;
-    if (Number(cardAttr1) > magic) {
-      return true;
-    }
-    if (Number(cardAttr2) > magic) {
-      return true;
-    }
-    if (Number(cardAttr3) > magic) {
+    if (Number(cardAttr1) > magic
+    || Number(cardAttr2) > magic || Number(cardAttr3) > magic) {
       return true;
     }
     return false;
-  }
+  };
 
   verifyAttr2 = () => {
     const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
     const magic = 0;
-    if (Number(cardAttr1) < magic) {
-      return true;
-    }
-    if (Number(cardAttr2) < magic) {
-      return true;
-    }
-    if (Number(cardAttr3) < magic) {
+    if (Number(cardAttr1) < magic || Number(cardAttr2) < magic
+    || Number(cardAttr3) < magic) {
       return true;
     }
     return false;
-  }
+  };
 
   onSaveButtonClick = (e) => {
     e.preventDefault();
-    const { cardName, cardDescription, cardAttr1, cardAttr2,
-      cardAttr3, cardImage, cardRare, cardTrunfo } = this.state;
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo } = this.state;
     const newCard = {
       cardName,
       cardDescription,
@@ -102,52 +105,91 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
     };
-    this.setState((state) => ({
-      cards: [newCard, ...state.cards],
-    }), () => {
-      this.setState({
-        cardName: '',
-        cardDescription: '',
-        cardAttr1: '0',
-        cardAttr2: '0',
-        cardAttr3: '0',
-        cardImage: '',
-        cardRare: 'normal',
-        cardTrunfo: false,
-        isSaveButtonDisabled: true,
-      }, () => {
-        if (cardTrunfo) {
-          this.setState({
-            hasTrunfo: true,
-          });
-        }
-      });
-    });
-  }
+    this.setState(
+      (state) => ({
+        cards: [newCard, ...state.cards],
+      }),
+      () => {
+        this.setState(
+          {
+            cardName: '',
+            cardDescription: '',
+            cardAttr1: '0',
+            cardAttr2: '0',
+            cardAttr3: '0',
+            cardImage: '',
+            cardRare: 'normal',
+            cardTrunfo: false,
+            isSaveButtonDisabled: true,
+          },
+          () => {
+            if (cardTrunfo) {
+              this.setState({
+                hasTrunfo: true,
+              });
+            }
+          },
+        );
+      },
+    );
+  };
 
   removeCard = ({ target }) => {
     const { cards } = this.state;
     const { name } = target;
     const card = cards.find((element) => element.cardName === name);
     if (card.cardTrunfo === true) {
-      this.setState({
-        hasTrunfo: false,
-      }, () => {
-        this.setState((PreviousState) => ({
-          cards: PreviousState.cards.filter((element) => element.cardName !== name),
-        }));
-      });
+      this.setState(
+        {
+          hasTrunfo: false,
+        },
+        () => {
+          this.setState((PreviousState) => ({
+            cards: PreviousState.cards.filter(
+              (element) => element.cardName !== name,
+            ),
+          }));
+        },
+      );
     } else {
       this.setState((PreviousState) => ({
-        cards: PreviousState.cards.filter((element) => element.cardName !== name),
+        cards: PreviousState.cards.filter(
+          (element) => element.cardName !== name,
+        ),
       }));
     }
-  }
+  };
+
+  onFilterChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  exibirCards = (cartas) => {
+    const cards = cartas.map((card) => (
+      <Card
+        key={ card.cardName }
+        cardName={ card.cardName }
+        cardDescription={ card.cardDescription }
+        cardAttr1={ card.cardAttr1 }
+        cardAttr2={ card.cardAttr2 }
+        cardAttr3={ card.cardAttr3 }
+        cardImage={ card.cardImage }
+        cardRare={ card.cardRare }
+        cardTrunfo={ card.cardTrunfo }
+        removeCard={ this.removeCard }
+      />
+    ));
+    return cards;
+  };
 
   render() {
-    const { cardName, cardDescription, cardAttr1, cardAttr2,
-      cardAttr3, cardImage, cardRare, cardTrunfo,
-      hasTrunfo, isSaveButtonDisabled, cards } = this.state;
+    const {
+      cardName, cardDescription, cardAttr2, cardAttr3, cardImage,
+      cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      cards, cardAttr1, filterInput } = this.state;
 
     return (
       <div>
@@ -179,20 +221,21 @@ class App extends React.Component {
           isPreview
         />
         <div>
-          {cards.map((card) => (
-            <Card
-              key={ card.cardName }
-              cardName={ card.cardName }
-              cardDescription={ card.cardDescription }
-              cardAttr1={ card.cardAttr1 }
-              cardAttr2={ card.cardAttr2 }
-              cardAttr3={ card.cardAttr3 }
-              cardImage={ card.cardImage }
-              cardRare={ card.cardRare }
-              cardTrunfo={ card.cardTrunfo }
-              removeCard={ this.removeCard }
-            />
-          ))}
+          <h3>Todas as Cartas</h3>
+          <p>Filtro de Busca</p>
+          <input
+            data-testid="name-filter"
+            value={ filterInput }
+            name="filterInput"
+            onChange={ this.onFilterChange }
+          />
+          {filterInput === ''
+            ? this.exibirCards(cards)
+            : this.exibirCards(
+              cards.filter((element) => element.cardName
+                .toLowerCase()
+                .includes(filterInput.toLowerCase())),
+            )}
         </div>
       </div>
     );
