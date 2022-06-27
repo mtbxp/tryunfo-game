1b63f251946e1f44b330682c2b15409233c878ec
Rubio <paulocezarrubio@gmail.com>
2022-06-27 19:31:20 -0300

    req 5quase completo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { number } from 'prop-types';
 import Form from './components/Form';
 import Card from './components/Card';
 
@@ -14,17 +15,57 @@ class App extends React.Component {
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
-      isSavedButtonDisabled: false,
-
+      isSavedButtonDisabled: true,
     };
-
     this.onInputChange = this.onInputChange.bind(this);
+    this.validaButton = this.validaButton.bind(this);
   }
 
   onInputChange({ target }) {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, this.validaButton);
+  }
+
+  validaButton() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+
+    const max = 90;
+    const min = 0;
+    const somamax = 210;
+
+    const soma = parseInt(cardAttr1, 10)
+    + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr3, 10);
+
+    const validaSoma = () => {
+      if (
+        soma <= somamax
+        && parseInt(cardAttr1, 10) <= max
+        && parseInt(cardAttr2, 10) <= max
+        && parseInt(cardAttr3, 10) <= max
+        && parseInt(cardAttr1, 10) >= 0
+        && parseInt(cardAttr2, 10) >= 0
+        && parseInt(cardAttr3, 10) >= 0
+      ) return true;
+    };
+    const cardArray = [cardName, cardDescription, cardImage];
+    const noInputs = cardArray.every((inputs) => inputs !== '');
+
+    const valida = validaSoma() && noInputs;
+
+    if (valida) {
+      this.setState({ isSavedButtonDisabled: false });
+    } else {
+      this.setState({ isSavedButtonDisabled: true });
+    }
   }
 
   render() {
@@ -37,10 +78,10 @@ class App extends React.Component {
       cardAttr3,
       cardRare,
       cardTrunfo,
+      isSavedButtonDisabled,
     } = this.state;
 
     return (
-
       <div>
         <h1>Tryunfo</h1>
         <Form
@@ -53,6 +94,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           onInputChange={ this.onInputChange }
+          isSavedButtonDisabled={ isSavedButtonDisabled }
         />
         <h2>Card</h2>
         <Card
