c7c03269d5d92d9c44a8c69c7a2f9897e350c768
Igor Sampaio <igorsamf@gmail.com>
2022-06-27 19:12:09 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   constructor() {
@@ -33,6 +34,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Tryunfo</h1>
+        <Card />
         <Form
           cardName={ cardName }
           cardDescription={ cardDescription }
