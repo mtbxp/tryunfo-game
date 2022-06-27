e5f428c32d42491e061347b8306a343f7d19069d
João Pedro <joaopetrussousa@gmail.com>
2022-06-27 19:26:46 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
@@ -22,17 +23,38 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.buttonDisabledTest);
   }
 
-  onSaveButtonClick = () => {
-    console.log('Teste');
+  buttonDisabledTest = () => {
+    // Trecho formatado após consulta no código da amiga Larissa-Menezes
+    const { cardName, cardDescription, cardAttr1,
+      cardAttr2, cardAttr3, cardImage, cardRare } = this.state;
+    const valorMax = 90;
+    const somatoriaValores = 210;
+    if (
+      cardName
+      && cardDescription
+      && Number(cardAttr1) >= 0
+      && Number(cardAttr1) <= valorMax
+      && Number(cardAttr2) >= 0
+      && Number(cardAttr2) <= valorMax
+      && Number(cardAttr3) >= 0
+      && Number(cardAttr3) <= valorMax
+      && Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= somatoriaValores
+      && cardImage
+      && cardRare
+    ) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   render() {
     const { cardName, cardDescription, cardAttr1,
       cardAttr2, cardAttr3, cardImage, cardRare,
-      cardTrunfo } = this.state;
+      cardTrunfo, isSaveButtonDisabled } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
@@ -46,7 +68,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           // hasTrunfo={ hasTrunfo }
-          // isSaveButtonDisabled={ isSaveButtonDisabled }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
           // onSaveButtonClick={ this.onSaveButtonClick }
         />
