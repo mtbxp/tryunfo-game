4fd08384a65091b187335fdd2c063ffd925ba58c
Guilherme Campos <guilhermeregular@gmail.com>
2022-06-26 21:35:57 -0300

    req 6

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      cards: [],
     };
   }
 
@@ -86,6 +87,38 @@ class App extends React.Component {
       return true;
     }
     return false;
+  }
+
+  onSaveButtonClick = (e) => {
+    e.preventDefault();
+    const { cardName, cardDescription, cardAttr1, cardAttr2,
+      cardAttr3, cardImage, cardRare, cardTrunfo } = this.state;
+    const newCard = {
+      cardName: [cardName],
+      cardDescription: [cardDescription],
+      cardAttr1: [cardAttr1],
+      cardAttr2: [cardAttr2],
+      cardAttr3: [cardAttr3],
+      cardImage: [cardImage],
+      cardRare: [cardRare],
+      cardTrunfo: [cardTrunfo],
+    };
+    this.setState((state) => ({
+      cards: [newCard, ...state.cards],
+    }), () => {
+      this.setState({
+        cardName: '',
+        cardDescription: '',
+        cardAttr1: '0',
+        cardAttr2: '0',
+        cardAttr3: '0',
+        cardImage: '',
+        cardRare: 'normal',
+        cardTrunfo: false,
+        hasTrunfo: false,
+        isSaveButtonDisabled: true,
+      });
+    });
   }
 
   render() {
