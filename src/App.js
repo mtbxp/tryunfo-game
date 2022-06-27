eb51ac2196113ce66a8b632947225f2340507751
Ricardo <ricardo_md_25@hotmail.com>
2022-06-27 15:19:18 -0300

    exercise 5

--- src/App.js
+++ src/App.js
@@ -14,13 +14,30 @@ class App extends React.Component {
       image: '',
       rare: '',
       trunfo: 'false',
+      isSaveButtonDisabled: 'true',
     };
   }
+
+  validateButton = () => {
+    const { name, description, attr1, attr2, attr3, image } = this.state;
+    const valueMax = 90;
+    const sum = Number(attr1) + Number(attr2) + Number(attr3);
+    const sumMax = 210;
+
+    if (!name || !description || !image
+      || attr1 > valueMax || attr2 > valueMax || attr3 > valueMax
+      || attr1 < 0 || attr2 < 0 || attr3 < 0 || sum > sumMax
+    ) {
+      return true;
+    }
+  };
 
   onInputChange = ({ target }) => {
     const { name, type } = target;
     const value = type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => this.setState({
+      isSaveButtonDisabled: this.validateButton(),
+    }));
   }
 
   render() {
@@ -33,12 +50,16 @@ class App extends React.Component {
       image,
       rare,
       trunfo,
+      isSaveButtonDisabled,
     } = this.state;
 
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form onInputChange={ this.onInputChange } />
+        <Form
+          onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+        />
         <Card
           cardName={ name }
           cardDescription={ description }
