348283b3c09139a71332f17c309a3e8aeff2508b
Breno Prado <breno.o.prado@hotmail.com>
2022-06-27 15:38:46 -0300

    Correções no eslint do css e na quantidade de caraacteres do nome e descriçãao :turtle:

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -76,6 +76,7 @@ class Form extends React.Component {
             data-testid="name-input"
             value={ cardName }
             onChange={ (event) => onInputChange(event) }
+            maxLength={ 22 }
           />
           <br />
         </label>
@@ -87,6 +88,7 @@ class Form extends React.Component {
             name="cardDescription"
             data-testid="description-input"
             value={ cardDescription }
+            maxLength={ 144 }
             onChange={ (event) => onInputChange(event) }
           />
           <br />
