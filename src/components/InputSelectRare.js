d2fe595dfa1cdee5ed5250170f6903b6709a8490
Danilo Martins <dansm86@outlook.com>
2022-06-27 10:04:45 -0300

    Added propTypes to InputSelectRare component

--- src/components/InputSelectRare.js
+++ src/components/InputSelectRare.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function InputSelectRare(props) {
   const { cardRare, onInputChange } = props;
@@ -20,3 +21,8 @@ export default function InputSelectRare(props) {
     </p>
   );
 }
+
+InputSelectRare.propTypes = {
+  cardRare: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
