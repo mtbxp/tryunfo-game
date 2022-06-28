db38d4e06c36038fe28f60e9e535d8999774d972
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-06-27 22:56:59 -0300

    Requizito 08 concluido. VAMOS COM TUDO :rocket: :octocat: :black_joker:

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,12 @@ class App extends React.Component {
     };
   }
 
+  containsTrufoInCards = () => {
+    const { cards } = this.state;
+    if (cards.length === 0) return false;
+    return cards.some((card) => card.trunfo);
+  };
+
   validateButton = () => {
     const { name, description, image, attr1, attr2, attr3 } = this.state;
 
@@ -81,6 +87,12 @@ class App extends React.Component {
     }));
   };
 
+  onDeleteButtonClick = (name) => {
+    this.setState((prev) => ({
+      cards: prev.cards.filter((card) => card.name !== name),
+    }), () => this.setState({ hasTrunfo: this.containsTrufoInCards() }));
+  };
+
   render() {
     const {
       name,
@@ -93,6 +105,7 @@ class App extends React.Component {
       trunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      cards,
     } = this.state;
     return (
       <div>
@@ -120,7 +133,24 @@ class App extends React.Component {
           cardImage={ image }
           cardRare={ rare }
           cardTrunfo={ trunfo }
+          onDeleteButtonClick={ this.onDeleteButtonClick }
+          preview={ false }
         />
+        {cards.map((card) => (
+          <Card
+            key={ card.name }
+            cardName={ card.name }
+            cardDescription={ card.description }
+            cardAttr1={ card.attr1 }
+            cardAttr2={ card.attr2 }
+            cardAttr3={ card.attr3 }
+            cardImage={ card.image }
+            cardRare={ card.rare }
+            cardTrunfo={ card.trunfo }
+            onDeleteButtonClick={ this.onDeleteButtonClick }
+            preview
+          />
+        ))}
       </div>
     );
   }
