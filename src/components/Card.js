58b67ee3658f6d7a6e081d1a30902375a6a7ba20
edmcorrea <edm.correa@hotmail.com>
2022-06-28 00:09:24 -0300

    adicionando correcoes

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,12 +15,12 @@ class Card extends React.Component {
       <>
         <p value={ cardName } data-testid="name-card">Card</p>
         <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <p value={ cardDescription }>descricao</p>
-        <p value={ cardAttr1 }>atrib1</p>
-        <p value={ cardAttr2 }>atrib1</p>
-        <p value={ cardAttr3 }>atrib1</p>
-        <p value={ cardRare }>rare</p>
-        {(cardTrunfo) ? <p>trunfo</p> : null}
+        <p value={ cardDescription } data-testid="description-card">descricao</p>
+        <p value={ cardAttr1 } data-testid="attr1-card">atrib1</p>
+        <p value={ cardAttr2 } data-testid="attr2-card">atrib1</p>
+        <p value={ cardAttr3 } data-testid="attr3-card">atrib1</p>
+        <p value={ cardRare } data-testid="rare-card">rare</p>
+        {(cardTrunfo) ? <p data-testid="trunfo-card">trunfo</p> : null}
       </>
     );
   }
