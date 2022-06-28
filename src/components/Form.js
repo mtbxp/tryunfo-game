876545fb584ea66f7a921e2bbbaa02bde3c19de9
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-06-28 16:58:24 -0300

    Requisitos 1 e 2 funcionando. Requisito 3 falta apenas concluir a renderização condicional. Arrumei tambem prop-types e htmlFor das labels

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
       cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled, onInputChange,
-       onSaveButtonClick} = this.props;
+      onSaveButtonClick } = this.props;
     return (
       <form>
-        <label>
+        <label htmlFor="name-input">
           Nome
           <input
             type="text"
@@ -16,7 +17,7 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label>
+        <label htmlFor="description-input">
           Descrição da Carta
           <textarea
             data-testid="description-input"
@@ -24,7 +25,7 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label>
+        <label htmlFor="attr1-input">
           Primeiro Atributo
           <input
             type="number"
@@ -33,7 +34,7 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label>
+        <label htmlFor="attr2-input">
           Segundo Atributo
           <input
             type="number"
@@ -42,7 +43,7 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label>
+        <label htmlFor="attr3-input">
           Terceiro Atributo
           <input
             type="number"
@@ -51,7 +52,7 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label>
+        <label htmlFor="image-input">
           Imagem
           <input
             type="text"
@@ -60,7 +61,7 @@ class Form extends React.Component {
             onChange={ onInputChange }
           />
         </label>
-        <label>
+        <label htmlFor="rare-input">
           Raridade
           <select data-testid="rare-input" value={ cardRare } onChange={ onInputChange }>
             <option value="normal">Normal</option>
@@ -68,7 +69,7 @@ class Form extends React.Component {
             <option value="muito raro">Muito Raro</option>
           </select>
         </label>
-        <label>
+        <label htmlFor="trunfo-input">
           Super Trunfo
           <input
             type="checkbox"
@@ -82,11 +83,26 @@ class Form extends React.Component {
           disabled={ isSaveButtonDisabled }
           onClick={ onSaveButtonClick }
         >
-            Salvar
+          Salvar
         </button>
       </form>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
