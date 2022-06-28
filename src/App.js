8ad8ddec793b32a8df894c96977261923f19528d
JoaoDobrowolski <joaodobrowolski@outlook.com>
2022-06-28 19:47:01 -0300

    1 a 8 ok

--- src/App.js
+++ src/App.js
@@ -82,6 +82,21 @@ class App extends React.Component {
           onSaveButtonClick={ this.handleSubmit }
         />
         <Card { ... this.state } />
+        <div>
+          { arrOfCards.map((card) => (
+            <Card
+              key={ card.cardName }
+              cardName={ card.cardName }
+              cardDescription={ card.cardDescription }
+              cardImage={ card.cardImage }
+              cardAttr1={ card.cardAttr1 }
+              cardAttr2={ card.cardAttr2 }
+              cardAttr3={ card.cardAttr3 }
+              cardRare={ card.cardRare }
+              cardTrunfo={ card.cardTrunfo }
+            />
+          )) }
+        </div>
       </div>
     );
   }
