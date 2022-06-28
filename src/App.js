5c18aa50f3505b6eaf09ff30530355bc0a80237c
Henrique Rosa <samuel_kuik@hotmail.com>
2022-06-28 10:05:25 -0300

    Finalizado Requisito 5

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
@@ -18,12 +18,62 @@ class App extends React.Component {
       isSaveButtonDisabled: true,
     };
     this.onInputChange = this.onInputChange.bind(this);
+    // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.validateInputs = this.validateInputs.bind(this);
   }
 
   onInputChange({ target }) {
     this.setState({
       [target.name]: target.value,
-    });
+    }, this.validateInputs);
+  }
+
+  /*   onSaveButtonClick() {
+    if (cardName !== '') {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
+  } */
+
+  validateInputs() {
+    const maxTotalAttributs = 210;
+    const maxAtribute = 90;
+    const minAtribute = 0;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+    } = this.state;
+    const parseAttr1 = parseInt(cardAttr1, 10);
+    const parseAttr2 = parseInt(cardAttr2, 10);
+    const parseAttr3 = parseInt(cardAttr3, 10);
+    const sumAttributs = parseAttr1 + parseAttr2 + parseAttr3;
+    if (
+      cardName.length > 0
+      && cardDescription.length > 0
+      && cardImage.length > 0
+      && cardRare
+      && parseAttr1 <= maxAtribute
+      && parseAttr2 <= maxAtribute
+      && parseAttr3 <= maxAtribute
+      && parseAttr1 >= minAtribute
+      && parseAttr2 >= minAtribute
+      && parseAttr3 >= minAtribute
+      && sumAttributs <= maxTotalAttributs
+    ) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
   }
 
   render() {
@@ -39,6 +89,7 @@ class App extends React.Component {
       hasTrunfo,
       isSaveButtonDisabled,
     } = this.state;
+
     return (
       <div className="Master-Content">
         <Form
