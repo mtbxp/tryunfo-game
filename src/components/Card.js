6fa9808c4b8dacd2c62388ffde829018af3e5fd9
Icaro Joel <icaromoura.programador@gmail.com>
2022-06-29 20:57:15 -0300

    test

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,6 +15,7 @@ class Card extends React.Component {
     } = this.props;
 
     return (
+      // yaeh
       <div className="card">
         <p data-testid="name-card">{ cardName }</p>
         <img src={ cardImage } alt={ cardName } data-testid="image-card" />
