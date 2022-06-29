6354c4ee088f567f797052aab7be3cabe299a8e5
Debora Franchi <deborafranchicarvalho@gmail.com>
2022-06-29 19:23:07 +0200

    requisito 5

--- src/App.js
+++ src/App.js
@@ -9,24 +9,56 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardImage: '',
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: true,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
 
     };
+  }
+
+  enableButton = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+    const maxValue = 90;
+    const sumValue = 210;
+
+    const sum = Number(cardAttr1)
+    + Number(cardAttr2)
+    + Number(cardAttr3)
+    <= sumValue;
+
+    const verifyInput = cardName !== ''
+    && cardDescription !== ''
+    && cardImage !== '';
+
+    const min = Number(cardAttr1) >= 0
+    && Number(cardAttr2) >= 0
+    && Number(cardAttr3) >= 0;
+
+    const max = Number(cardAttr1) <= maxValue
+    && Number(cardAttr2) <= maxValue
+    && Number(cardAttr3) <= maxValue;
+    if (verifyInput && min && max && sum) {
+      return false;
+    }
+    return true;
   }
 
   onInputChange =({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    });
+    this.setState({ [name]: value },
+      () => this.setState({ isSaveButtonDisabled: this.enableButton() }));
   }
 
   render() {
