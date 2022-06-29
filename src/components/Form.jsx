d68b74cb5c6e963d2c4a251ace6677dcfe6ab7f8
Isabelle Strojake <strojake@gmail.com>
2022-06-29 11:55:32 -0300

    correções e css do form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,80 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Form extends React.Component {
+export default class Form extends Component {
   render() {
-    const {
-      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
-      onInputChange, onSaveButtonClick,
-    } = this.props;
     return (
-      <div className="form-container">
+      <form>
         <label htmlFor="name">
-          Nome
-          <input
-            type="text"
-            data-testid="name-input"
-            value={ cardDescription }
-          />
+          <input type="text" data-testid="name-input" name="name" id="name" />
         </label>
         <label htmlFor="description">
-          Descrição
-          <textarea
+          <input
+            type="textarea"
             data-testid="description-input"
+            name="description"
+            id="description"
           />
         </label>
         <label htmlFor="attr1">
-          Atributo 1
           <input
             type="number"
             data-testid="attr1-input"
+            name="attr1"
+            id="attr1"
           />
         </label>
         <label htmlFor="attr2">
-          Atributo 2
           <input
             type="number"
             data-testid="attr2-input"
+            name="attr2"
+            id="attr2"
           />
         </label>
         <label htmlFor="attr3">
-          Atributo 3
           <input
             type="number"
             data-testid="attr3-input"
+            name="attr3"
+            id="attr3"
           />
         </label>
-        <label htmlFor="img">
-          URL da Imagem
+        <label htmlFor="image">
           <input
             type="text"
             data-testid="image-input"
+            name="image"
+            id="image"
           />
         </label>
-        <label htmlFor="rarity">
-          Raridade
-          <select data-testid="rare-input" id="rarity">
-            <option value="normal"> Normal </option>
-            <option value="raro"> Raro </option>
-            <option value="muito raro"> Muito Raro </option>
+
+        <label htmlFor="rare">
+          <select name="rare" id="rare" data-testid="rare-input">
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
           </select>
         </label>
-        <label htmlFor="super-trunfo">
-          Super Trunfo
+
+        <label htmlFor="trunfo">
           <input
             type="checkbox"
+            name="trunfo"
+            id="trunfo"
             data-testid="trunfo-input"
           />
         </label>
-        <button
-          type="button"
-          data-testid="save-button"
-        >
+
+        <button type="submit" data-testid="save-button">
           Salvar
         </button>
-      </div>
+      </form>
     );
   }
 }
-
-export default Form;
