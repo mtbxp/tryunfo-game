f7d1d5319410e64f54e7fe3432a4be73408c501d
rogelins <rogelins@gmail.com>
2022-06-28 21:16:26 -0300

    requisito 08 finalizado

--- src/App.js
+++ src/App.js
@@ -100,6 +100,7 @@ class App extends React.Component {
       cardTrunfo,
       isSaveButtonDisabled,
       hasTrunfo,
+      deckCards,
     } = this.state;
 
     return (
@@ -127,6 +128,19 @@ class App extends React.Component {
           cardAttr3={ cardAttr3 }
           cardTrunfo={ cardTrunfo }
         />
+        <h1>Deck Cards</h1>
+        {deckCards.map((card, index) => (<Card
+          key={ index }
+          cardName={ card.cardName }
+          cardDescription={ card.cardDescription }
+          cardAttr1={ card.cardAttr1 }
+          cardAttr2={ card.cardAttr2 }
+          cardAttr3={ card.cardAttr3 }
+          cardImage={ card.cardImage }
+          cardRare={ card.cardRare }
+          cardTrunfo={ card.cardTrunfo }
+        />))}
+
       </div>
     );
   }
