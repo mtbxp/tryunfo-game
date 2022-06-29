b58cf4087a5157a4530b17fae273bb3d5316eeae
Cesar Xavier <cxavier.vet@gmail.com>
2022-06-29 01:48:48 -0300

    Requisito 3 Completo: Renderizando Card em App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -7,10 +8,10 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card cardName={ cardName } />
       </div>
     );
   }
 }
 
 export default App;
-// Iniciando Projeto
