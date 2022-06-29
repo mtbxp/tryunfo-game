cae2133486e95a7891fc37733eebb713d3b4390b
Isabelle Strojake <strojake@gmail.com>
2022-06-29 11:48:23 -0300

    requisito 1

---
+++ src/components/Form.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    const {
+      cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo, hasTrunfo, isSaveButtonDisabled,
+      onInputChange, onSaveButtonClick,
+    } = this.props;
+    return (
+      <div className="form-container">
+        <label htmlFor="name">
+          Nome
+          <input
+            type="text"
+            data-testid="name-input"
+            value={ cardDescription }
+          />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <textarea
+            data-testid="description-input"
+          />
+        </label>
+        <label htmlFor="attr1">
+          Atributo 1
+          <input
+            type="number"
+            data-testid="attr1-input"
+          />
+        </label>
+        <label htmlFor="attr2">
+          Atributo 2
+          <input
+            type="number"
+            data-testid="attr2-input"
+          />
+        </label>
+        <label htmlFor="attr3">
+          Atributo 3
+          <input
+            type="number"
+            data-testid="attr3-input"
+          />
+        </label>
+        <label htmlFor="img">
+          URL da Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+          />
+        </label>
+        <label htmlFor="rarity">
+          Raridade
+          <select data-testid="rare-input" id="rarity">
+            <option value="normal"> Normal </option>
+            <option value="raro"> Raro </option>
+            <option value="muito raro"> Muito Raro </option>
+          </select>
+        </label>
+        <label htmlFor="super-trunfo">
+          Super Trunfo
+          <input
+            type="checkbox"
+            data-testid="trunfo-input"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="save-button"
+        >
+          Salvar
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Form;
