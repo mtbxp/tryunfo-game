c12306321177c00e2a2e25aa9274ef0ac9686e9c
Carlos Augusto <caarlos.san@gmail.com>
2022-06-29 11:18:59 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -18,15 +18,55 @@ class App extends React.Component {
       cardRare: '',
       cardTrunfo: false,
       hasTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
   onInputChange(event) {
     const { name, value } = event.target;
-    console.log('ok');
     this.setState({
       [name]: value,
-    });
+    }, () => this.activeBtn());
+  }
+
+  activeBtn = () => {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare } = this.state;
+
+    const stringsInputs = () => [
+      cardName !== '',
+      cardDescription !== '',
+      cardImage !== '',
+      cardRare !== ''].every((element) => element === true);
+
+    const maxAttr = 90;
+
+    const inputsNumbers = () => [
+      (cardAttr1 <= maxAttr && cardAttr1 >= 0),
+      (cardAttr2 <= maxAttr && cardAttr2 >= 0),
+      (cardAttr3 <= maxAttr && cardAttr3 >= 0),
+    ].every((element) => element === true);
+
+    const sumAttr = 210;
+
+    const arrayNumbers = () => [
+      (+cardAttr1) + (+cardAttr2) + (+cardAttr3),
+    ];
+
+    if (stringsInputs() && inputsNumbers() && arrayNumbers()[0] <= sumAttr) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    } else {
+      (this.setState({
+        isSaveButtonDisabled: true,
+      }));
+    }
   }
 
   render() {
@@ -40,6 +80,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
 
     return (
@@ -56,6 +97,7 @@ class App extends React.Component {
           cardTrunfo={ cardTrunfo }
           hasTrunfo={ hasTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ cardName }
