3de0e9cded2148a5ee585ce37bcc3050777b0dcd
Malrus-dev <maelsobfer@gmail.com>
2022-06-30 06:54:29 -0300

    3 finalizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   constructor() {
@@ -23,6 +24,7 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
       </div>
     );
   }
