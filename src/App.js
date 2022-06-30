b61a66124311da6140ddb45c662168ca1a15df84
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-06-29 22:26:05 -0300

    Adicionando requisito 8

--- src/App.js
+++ src/App.js
@@ -119,6 +119,19 @@ class App extends React.Component {
           cardRare={ value.cardRare }
           cardTrunfo={ value.cardTrunfo }
         />
+        {value.savedCards.map((card) => (
+          <Card
+            cardName={ card.cardName }
+            cardDescription={ card.cardDescription }
+            cardAttr1={ card.cardAttr1 }
+            cardAttr2={ card.cardAttr2 }
+            cardAttr3={ card.cardAttr3 }
+            cardImage={ card.cardImage }
+            cardRare={ card.cardRare }
+            cardTrunfo={ card.cardTrunfo }
+            key={ card.cardName }
+          />
+        ))}
       </div>
     );
   }
