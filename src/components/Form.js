b585fd27c539e1914cc27ce12d184c8e45ac7b8f
Artur Vidor <vidorartur@gmail.com>
2022-06-29 21:41:28 -0300

    Primeiro Exercicio-2

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,36 +5,44 @@ class Form extends React.Component {
     return (
       <div>
         <form>
-          <input type="text" data-testid="name-input" />
-          <br />
-          <textarea data-testid="description-input" />
-          <br />
-          <input type="number" data-testid="attr1-input" />
-          <br />
-          <input type="number" data-testid="attr2-input" />
-          <br />
-          <input type="number" data-testid="attr3-input" />
-          <br />
-          <input type="text" data-testid="image-input" />
-          <br />
-          <select data-testid="rare-input">
-            <option>
-              Normal
-            </option>
-            <option>
-              Raro
-            </option>
-            <option>
-              Muito Raro
-            </option>
-          </select>
-          <br />
+          <label htmlFor="card_name">
+            <input type="text" data-testid="name-input" id="card_name" />
+          </label>
+          <label htmlFor="card_description">
+            <textarea data-testid="description-input" id="card_description" />
+          </label>
+          <label htmlFor="card_attr1">
+            <input type="number" data-testid="attr1-input" id="card_attr1" />
+          </label>
+          <label htmlFor="card_attr2">
+            <input type="number" data-testid="attr2-input" id="card_attr2" />
+          </label>
+          <label htmlFor="card_attr3">
+            <input type="number" data-testid="attr3-input" id="card_attr3" />
+          </label>
+          <label htmlFor="card_image">
+            <input type="text" data-testid="image-input" id="card_image" />
+          </label>
+          <label htmlFor="card_rare">
+            <select data-testid="rare-input" id="card_rare">
+              <option>
+                Normal
+              </option>
+              <option>
+                Raro
+              </option>
+              <option>
+                Muito Raro
+              </option>
+            </select>
+          </label>
           <label htmlFor="super_trunfo">
             <input type="checkbox" data-testid="trunfo-input" id="super_trunfo" />
             Super Trunfo
           </label>
-          <br />
-          <button data-testid="save-button" type="button">Salvar</button>
+          <label htmlFor="button">
+            <input data-testid="save-button" type="button" id="button" value="Salvar" />
+          </label>
         </form>
       </div>
     );
