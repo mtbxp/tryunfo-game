89b59c585800d3d4f46a75d030204777fda4dc3f
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-30 11:48:18 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -13,19 +13,26 @@ class App extends React.Component {
       attr2: '',
       attr3: '',
       image: '',
-      rare: 'raro',
+      rare: '',
       trunfo: false,
       button: true,
     };
   }
 
   habilitButton = () => {
-    const { name, description, image, attr1, attr2, attr3 } = this.state;
-    const inputsText = [name, description, image];
+    const { name, description, image, rare, attr1, attr2, attr3 } = this.state;
+    const inputsText = [name, description, image, rare];
     const textArea = inputsText.every((area) => area.length > 0);
+    const attrText = [Number(attr1), Number(attr2), Number(attr3)];
+    const maxPointAttr = 90;
+    const maxPointAllsAttr = 210;
+    const minPoint = 0;
+    const attrArea = attrText.reduce((acc, cur) => Number(acc) + Number(cur), 0);
+    const attrSum = attrText.every((attr) => attr >= minPoint
+    && attr <= maxPointAttr && attrArea <= maxPointAllsAttr);
 
     this.setState({
-      button: !(textArea),
+      button: !(textArea && attrSum),
     });
   }
 
