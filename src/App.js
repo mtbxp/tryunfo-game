67305c04e48cdab0975033f516ff7eef55e34daa
Marcos Robson de Oliveira <robson_marcos1202@hotmail.com>
2022-07-01 15:25:00 -0500

    requisito 8

--- src/App.js
+++ src/App.js
@@ -125,6 +125,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      saveCard,
       isSaveButtonDisabled,
     } = this.state;
     return (
@@ -154,6 +155,19 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {saveCard.map((element) => (
+          <Card
+            key={ element.cardName }
+            cardName={ element.cardName }
+            cardDescription={ element.cardDescription }
+            cardAttr1={ element.cardAttr1 }
+            cardAttr2={ element.cardAttr2 }
+            cardAttr3={ element.cardAttr3 }
+            cardImage={ element.cardImage }
+            cardRare={ element.cardRare }
+            cardTrunfo={ element.cardTrunfo }
+          />
+        ))}
       </div>
     );
   }
