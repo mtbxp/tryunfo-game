02c85d205cb90f3c359e6b3d7d16028d97067f9e
Malrus-dev <maelsobfer@gmail.com>
2022-07-01 14:13:09 -0300

    5 precisa de revisão

--- src/App.js
+++ src/App.js
@@ -11,20 +11,44 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
 
   handleChange = ({ target }) => {
     const { name, type } = target;
+    const MAX_ATT = 90;
+    const MAX_POINTS = 210;
     const value = type === 'checkbox' ? target.checked : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => {
+      const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+        cardImage } = this.state;
+      console.log('cardAtt1', parseInt(cardAttr1, 10));
+      console.log('cardAtt2', parseInt(cardAttr2, 10));
+      console.log('cardAtt3', parseInt(cardAttr3, 10));
+      console.log('cardName', cardName.length);
+      console.log('cardDescription', cardDescription.length);
+      console.log('cardImage', cardImage.length);
+
+      if (cardName.length > 0 && cardDescription.length > 0 && cardImage.length > 0
+        && parseInt(cardAttr1, 10) <= MAX_ATT && parseInt(cardAttr2, 10) <= MAX_ATT
+        && parseInt(cardAttr3, 10) <= MAX_ATT && parseInt(cardAttr1, 10) > 0
+        && parseInt(cardAttr2, 10) > 0 && parseInt(cardAttr3, 10) > 0
+        && parseInt(cardAttr1, 10) + parseInt(cardAttr2, 10) + parseInt(cardAttr3, 10)
+        <= MAX_POINTS) {
+        this.setState({ isSaveButtonDisabled: false });
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
+    });
+    // const LIST = ['cardName', 'cardDescription', 'cardAttr1', 'cardAttr2', 'cardAttr3',
+    //  'cardImage', 'cardRare'];
   };
 
   handleSubmit = (e) => {
