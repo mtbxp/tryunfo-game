d24dabd1d9f5519e0c7f926338d567be1253643a
Rejane Raquel <rejane.lima87@gmail.com>
2022-07-02 16:44:08 -0300

    Requisito 1 Incompleto 8/9

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,15 +8,40 @@ class Form extends React.Component {
           Nome da Carta
           <input type="text" data-testid="name-input" />
         </label>
-        <input type="text-area" data-testid="description-input" />
-        <input type="number" data-testid="attr1-input" />
-        <input type="number" data-testid="attr2-input" />
-        <input type="number" data-testid="attr3-input" />
-        <input type="text" data-testid="image-input" />
-        <input type="select" data-testid="rare-input" />
-        <input type="checkbox" data-testid="trunfo-input" />
-        <button type="button" data-testid="save-button">Salvar</button>
-
+        <label htmlFor="description">
+          Descrição
+          <input type="text-area" data-testid="description-input" />
+        </label>
+        <label htmlFor="atr1">
+          Atributo 1
+          <input type="number" data-testid="attr1-input" />
+        </label>
+        <label htmlFor="atr2">
+          Atributo 2
+          <input type="number" data-testid="attr2-input" />
+        </label>
+        <label htmlFor="atr3">
+          Atributo 3
+          <input type="number" data-testid="attr3-input" />
+        </label>
+        <label htmlFor="image">
+          Imagem
+          <input type="text" data-testid="image-input" />
+        </label>
+        <label htmlFor="rare">
+          <select data-testid="rare-input">
+            <options value="normal">normal</options>
+            <options value="raro">raro</options>
+            <options value="muito-raro">muito raro</options>
+          </select>
+        </label>
+        <label htmlFor="super-trunfo">
+          <input type="checkbox" data-testid="trunfo-input" />
+        </label>
+        <label htmlFor="save-button">
+          Salvar
+          <button type="button" data-testid="save-button" />
+        </label>
       </>
     );
   }
