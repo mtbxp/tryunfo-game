ea7e374ade16f877fd5a070b8f005794f1495dc3
Diego Mello <diego013@gmail.com>
2022-07-03 18:31:48 -0300

    Criando estrutura de Form nos componentes para requisitos 1 e 2

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FormInputs from './FormInputs';
+
+class Form extends React.Component{
+  render(){
+
+  }
+}
+
+Form.propTypes = {
+
+};
+
+export default Form;
