d63be58d149d08d8b9263f00233b175f24024270
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-07-03 15:48:08 -0300

    Requirements 3, 4 and 5

--- src/App.js
+++ src/App.js
@@ -9,22 +9,54 @@ class App extends React.Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
+
+  verifyLength = (...inputsForm) => (inputsForm
+    .every((input) => input.length > 0));
+
+  verifyMinAndMax = (min, max, ...attrs) => (attrs
+    .every((attr) => Number(attr) >= min && Number(attr) <= max));
+
+  verifySum = (maxSum, ...attrs) => (attrs
+    .reduce((total, attr) => total + Number(attr), 0) <= maxSum);
+
+  verifyInputs = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+    } = this.state;
+
+    const validInput = this.verifyLength(cardName, cardDescription, cardImage);
+
+    const maxAttr = 90;
+    const validAttr = (0, maxAttr, cardAttr1, cardAttr2, cardAttr3);
+
+    const maxSum = 210;
+    const validSum = this.verifySum(maxSum, cardAttr1, cardAttr2, cardAttr3);
+
+    return (validInput && validAttr && validSum);
+  };
 
   onInputChange = ({ target }) => {
     const { name, type, checked, value } = target;
     const changeValue = type === 'checkbox' ? checked : value;
     this.setState({
       [name]: changeValue,
-    });
+    }, () => this.setState({
+      isSaveButtonDisabled: this.verifyInputs(),
+    }));
   }
 
   render() {
@@ -37,7 +69,6 @@ class App extends React.Component {
       cardImage,
       cardRare,
       cardTrunfo,
-      // hasTrunfo,
       isSaveButtonDisabled,
       onSaveButtonClick,
     } = this.state;
@@ -66,9 +97,6 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
-          onSaveButtonClick={ onSaveButtonClick }
-          onInputChange={ this.onInputChange }
         />
       </div>
     );
