8d86b1de0c72c768f6407226e2250c242c92e4f8
Bambirra <ivan.bambirra@engenharia.ufjf.br>
2022-07-04 19:07:20 -0300

    esqueci de fazer commits ate o requesito 7

--- src/App.js
+++ src/App.js
@@ -1,15 +1,144 @@
-import React from 'react';
-import Form from './components/Form';
+import React, { Component } from 'react';
 import './index.css';
 import Card from './components/Card';
+import Form from './components/Form';
 
-class App extends React.Component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+      cards: [],
+    };
+  }
+
+verifyFild = () => {
+  const {
+    cardName,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+    cardRare,
+  } = this.state;
+  const pontosPorCarta = 90;
+  const somaDasCartas = 210;
+  const somaDosAtributos = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+  if (cardName !== ''
+      && cardDescription !== ''
+      && cardImage !== ''
+      && cardRare !== ''
+      && cardAttr1 >= 0
+      && cardAttr2 >= 0
+      && cardAttr3 >= 0
+      && cardAttr1 <= pontosPorCarta
+      && cardAttr2 <= pontosPorCarta
+      && cardAttr3 <= pontosPorCarta
+      && somaDosAtributos <= somaDasCartas) {
+    this.setState({
+      isSaveButtonDisabled: false,
+    });
+  } else {
+    this.setState({
+      isSaveButtonDisabled: true,
+    });
+  }
+  console.log(somaDosAtributos);
+}
+
+  onInputChange = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.verifyFild());
+  }
+
+  onSaveButtonClick = (e) => {
+    e.preventDefault();
+
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo } = this.state;
+
+    const allCards = {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo,
+    };
+
+    this.setState((preventState) => ({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cards: [...preventState.cards, allCards],
+    }));
+  };
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+    } = this.state;
+
     return (
-      <div>
-        <Form />
-        <Card />
-      </div>
+      <section>
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
+
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
+      </section>
     );
   }
 }
