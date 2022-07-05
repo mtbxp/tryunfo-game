19ec61f4254837588de10a20b4befff93fa19ec8
mtbxp <marcellotb2@gmail.com>
2022-07-05 10:28:04 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -28,51 +28,83 @@ class App extends React.Component {
     });
   }
 
-  render() {
-    const {
-      cardName,
-      cardDescription,
-      cardAttr1,
-      cardAttr2,
-      cardAttr3,
-      cardImage,
-      cardRare,
-      cardTrunfo,
-      isSaveButtonDisabled,
-      onSaveButtonClick,
-    } = this.state;
+disableButton = () => {
+  const {
+    cardName,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+  } = this.state;
 
-    return (
-      <>
-        <Form
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
-          onInputChange={ this.onInputChange }
-          onSaveButtonClick={ onSaveButtonClick }
-        />
-        <Card
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
-          onInputChange={ this.onInputChange }
-          onSaveButtonClick={ onSaveButtonClick }
-        />
-      </>
-    );
+  const minAttr = 0;
+  const maxAttr = 90;
+  const max = 210;
+
+  const soma = (Math.round(cardAttr1)
+    + Math.round(cardAttr2) + Math.round(cardAttr3)) <= max;
+
+  const maxInput = Math.round(cardAttr1) <= maxAttr
+  && Math.round(cardAttr2) <= maxAttr
+  && Math.round(cardAttr3) <= maxAttr;
+
+  const minInput = Math.round(cardAttr1) >= minAttr
+  && Math.round(cardAttr2) >= minAttr
+  && Math.round(cardAttr3) >= minAttr;
+
+  if (cardName.length > 0 && cardImage.length > 0 && cardDescription.length > 0
+    && soma && maxInput && minInput) {
+    return false;
   }
+  return true;
+}
+
+render() {
+  const {
+    cardName,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+    cardRare,
+    cardTrunfo,
+    isSaveButtonDisabled,
+    onSaveButtonClick,
+  } = this.state;
+
+  return (
+    <>
+      <Form
+        cardName={ cardName }
+        cardDescription={ cardDescription }
+        cardAttr1={ cardAttr1 }
+        cardAttr2={ cardAttr2 }
+        cardAttr3={ cardAttr3 }
+        cardImage={ cardImage }
+        cardRare={ cardRare }
+        cardTrunfo={ cardTrunfo }
+        isSaveButtonDisabled={ this.disableButton() }
+        onInputChange={ this.onInputChange }
+        onSaveButtonClick={ onSaveButtonClick }
+      />
+      <Card
+        cardName={ cardName }
+        cardDescription={ cardDescription }
+        cardAttr1={ cardAttr1 }
+        cardAttr2={ cardAttr2 }
+        cardAttr3={ cardAttr3 }
+        cardImage={ cardImage }
+        cardRare={ cardRare }
+        cardTrunfo={ cardTrunfo }
+        isSaveButtonDisabled={ isSaveButtonDisabled }
+        onInputChange={ this.onInputChange }
+        onSaveButtonClick={ onSaveButtonClick }
+      />
+    </>
+  );
+}
 }
 
 export default App;
